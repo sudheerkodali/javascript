357367f079f6f0ed590eadd3db0e039b905879ab
sudheer chowdari kodali <45354847+sudheerkodali@users.noreply.github.com>
2023-09-14 15:56:28 +0530

    Add files via upload

---
+++ RR/script/main.js
@@ -0,0 +1,9 @@
+//output in javascript
+
+// console.log("Hello sudheer chowdari");
+// console.log("welcome to the full stack developer");
+
+alert("hello, sudheer are you perfect in full-stack developer");
+
+
+document.write("READY, for programming");
