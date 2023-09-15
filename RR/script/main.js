38ad0d644119e1a6acc989612e6112dd296bf9b8
sudheer chowdari kodali <45354847+sudheerkodali@users.noreply.github.com>
2023-09-15 11:26:19 +0530

    Update main.js

--- RR/script/main.js
+++ RR/script/main.js
@@ -7,3 +7,70 @@ alert("hello, sudheer are you perfect in full-stack developer");
 
 
 document.write("READY, for programming");
+
+// let declaring the varibles
+let age;
+age = 24;
+console.log(age);
+
+// another type of variables:"string"
+
+let name = "sudheer";
+console.log(name);
+
+// boolean: true or false
+
+let student = false;
+console.log(student);
+
+//operators =,-,*,/,%
+
+let number1 = 50;
+let number2 = 30;
+
+let sum = number1 + number2;
+console.log(sum);
+
+//expression operator
+
+let expression = 2 + 3 * (10 - 5);
+console.log(expression);
+
+//operator precedure
+/*
+1. paranthesis ()
+2. exponents 2^4, 2^2
+3. multiplication or division
+4. addition or subraction
+*/
+let age = 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
